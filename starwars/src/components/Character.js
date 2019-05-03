1482068d6a491333670299009ec6f46fe6271262
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-03 10:36:25 -0600

    identities and character js

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Identity from './Identity';
+
+const Character = props => {
+    return (
+        <div className="character-card">
+        <img src={props.character.img} alt="" />
+        <div className="character-info">
+            <h3>{props.character.name}</h3>
+            <Identity character={props.student} />
+            <p>
+                <strong>Birth Year:</strong> {props.character.birth}
+            </p>
+            <p>
+                <strong>Homeworld:</strong> {props.character.homeWorld}
+            </p>
+            <p>
+                <strong>Species:</strong> {props.character.species}
+            </p>
+            <p>
+                <strong>Films:</strong> {props.character.films}
+            </p>
+        </div>
+        </div>
+    );
+};
+
+export default Character;
